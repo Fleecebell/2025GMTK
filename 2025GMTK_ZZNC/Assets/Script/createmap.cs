39a082946a768fd644cfe661b24d71940c1adc66
Fleecebell <2126401535@qq.com>
2025-08-01 14:28:51 +0800

    第一关地图部分完成

--- 2025GMTK_ZZNC/Assets/Script/createmap.cs
+++ 2025GMTK_ZZNC/Assets/Script/createmap.cs
@@ -22,15 +22,33 @@ public class createmap : MonoBehaviour
             for (int y = 0; y < mapHeight; y++)
             {
                 // 随机选择一个房间预制体
-                int randomIndex = Random.Range(0, roomPrefabs.Length);
-                GameObject selectedRoomPrefab = roomPrefabs[randomIndex];
+                GameObject selectedRoomPrefab = SelectRoomPrefab();
 
                 // 实例化房间预制体
-                GameObject roomInstance = Instantiate(selectedRoomPrefab, new Vector3(x * 18.13f, y * 10.35f, 0), Quaternion.identity);
+                GameObject roomInstance = Instantiate(selectedRoomPrefab, new Vector3(x * 18.135f, y * 10.355f, 0), Quaternion.identity);
 
                 // 如果需要，可以添加到某个父对象下
                 roomInstance.transform.parent = transform;
             }
+        }
+    }
+
+    GameObject SelectRoomPrefab()
+    {
+        int randomIndex = Random.Range(0, 100); // 生成0到99的随机数
+
+        if (randomIndex < 50) // 50%的概率选择普通房间
+        {
+            int roomIndex = Random.Range(0, 6); // 选择索引0到5的普通房间
+            return roomPrefabs[roomIndex];
+        }
+        else if (randomIndex < 75) // 25%的概率选择商店（索引6）
+        {
+            return roomPrefabs[6];
+        }
+        else // 25%的概率选择宝箱房（索引7）
+        {
+            return roomPrefabs[7];
         }
     }
 }
